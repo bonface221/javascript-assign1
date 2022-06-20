2948ed4904e51ba1aa0648491ce912e381de6ef3
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-20 22:27:45 +0300

    ft:added the templates and js

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,5 @@
-const greetings = () =>{
+const greetings = () => {
+	
 	const value = document.getElementById('text').value;
 	let para = document.getElementById("demo");
 	para.textContent=`Hello ${value}, welcome to the bootcamp`;
