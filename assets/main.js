3829768f848ffbeed69bcd49272c607a3a4188c2
[bonface221] <[mainaboniface221@gmail.com]>
2022-06-20 23:28:48 +0300

    final changes

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,5 @@
-const greetings = () => {
-	
-	const value = document.getElementById('text').value;
+let btn = document.getElementById("btn").addEventListener("click", () => {
+	const value = document.getElementById("text").value;
 	let para = document.getElementById("demo");
-	para.textContent=`Hello ${value}, welcome to the bootcamp`;
-}
+	para.textContent = `Hello ${value}, welcome to the bootcamp`;
+});
